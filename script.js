38c5cd90602862817d0af9a0bc2a627271c0a168
nekqadamovsino-spec <nekqadamovsino@gmail.com>
2026-06-21 21:09:58 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbw6Rkk0s1gQPOfjUV5vUoc7RB_5q59ylUSrj3-zsv3x9nreqEOPifO457vYVmHyygcmig/exec";
 const weddingDate = new Date('2026-07-15T16:00:00+05:00');
 function tick(){
   const now = new Date();
@@ -24,13 +25,30 @@ document.querySelectorAll('.reveal').forEach(el => observer.observe(el));
 
 const guestForm = document.getElementById('guestForm');
 const formStatus = document.getElementById('formStatus');
+
 if (guestForm) {
   guestForm.addEventListener('submit', function(e){
     e.preventDefault();
+
     const data = new FormData(guestForm);
     const name = data.get('name');
     const presence = data.get('presence');
-    formStatus.textContent = `${name}, спасибо! Ответ записан: ${presence}.`;
+
+    formStatus.textContent = 'Отправляем...';
+
+    fetch(GOOGLE_SCRIPT_URL, {
+      method: 'POST',
+      mode: 'no-cors',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        name: name,
+        attendance: presence
+      })
+    });
+
+    formStatus.textContent = `${name}, спасибо! Ответ записан.`;
     guestForm.reset();
   });
 }
